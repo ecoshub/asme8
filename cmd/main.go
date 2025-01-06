f040e237f07f8d5fd2e0c55fad0e4801d58d0176
ecoshub <ocakemrecan@gmail.com>
2025-01-07 02:27:42 +0300

    feat: nop instruction implemented

--- cmd/main.go
+++ cmd/main.go
@@ -14,48 +14,21 @@ var (
 func main() {
 	flag.Parse()
 
-	c := comp.New()
-	program := playGroundJump()
-	if *flagFileBin != "" {
-		var err error
-		program, err = os.ReadFile(*flagFileBin)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+	if *flagFileBin == "" {
+		fmt.Println("error no input file. please provide a executable (bin)")
+		return
 	}
 
-	fmt.Println(program)
+	program, err := os.ReadFile(*flagFileBin)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
+	c := comp.New()
 	c.SetDebug(true)
 	c.SetVerbose(true)
 	c.SetDelayMS(100)
 	c.Put(0, program)
 	c.Run()
-}
-
-func playGroundJump() []byte {
-	return []byte{
-		// mov a, #0
-		0x41, 0x00, 0x00,
-		// mov b, #1
-		0x41, 0x01, 0x01,
-		// add a, b
-		0x30, 0x10,
-		// jmp #6
-		0x10, 0x06, 0x00,
-	}
-}
-
-func playGround() []byte {
-	return []byte{
-		// mov a, #30
-		0x41, 0x00, 0x30,
-		// mov b, #31
-		0x41, 0x01, 0x31,
-		// mov c, a
-		0x40, 0x20,
-		// mov c, a
-		0x30, 0x21,
-	}
 }

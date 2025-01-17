020f1dae91e2e26fbe6b19ac1b8507a9acbc98c1
ecoshub <ocakemrecan@gmail.com>
2025-01-17 23:45:17 +0300

    fix: keyboard read addresses changed. ss and small description added

---
+++ README.md
@@ -0,0 +1,5 @@
+# E8 Computer Emulator & Assembler
+E8 is a personal custom design computer.
+this repo contains its assembler and emulator.
+
+![home](ss.png)
---
+++ assembler/examples/hello_world.asm
@@ -0,0 +1,21 @@
+ADDR_CHAR_OUT=0x7000
+
+start:
+    mov b, 0
+loop:
+    mov a, [message+b]
+    cmp a, 0
+    jz done
+    jsr print_char
+    inc b
+    jmp loop
+
+print_char:
+    mov [ADDR_CHAR_OUT+b], a
+    rts
+
+done:
+    brk
+
+message:
+.byte 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', 0

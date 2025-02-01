c5a8598fc1e2cd867be526fec45cea0615186d73
ecoshub <ocakemrecan@gmail.com>
2025-02-02 01:49:54 +0300

    fix: index pointer added to flag panel

--- emulator/src/comp/log.go
+++ emulator/src/comp/log.go
@@ -94,7 +94,7 @@ func (c *Comp) LogState() {
 	c.LogfFlagIndexWithStyle(0, DefaultStyle6, "# Registers:")
 	c.LogfFlagIndexWithStyle(1, DefaultStyle6, "Z S P C O I X X")
 	c.LogfFlagIndexWithStyle(2, DefaultStyle6, "%s", c.status)
-	c.LogfFlagIndexWithStyle(3, DefaultStyle7, "A  B  C  D")
+	c.LogfFlagIndexWithStyle(3, DefaultStyle7, "A  B  C  D  IH IL")
 	c.LogfFlagIndexWithStyle(4, DefaultStyle7, "%s", c.registers)
 	c.LogfFlagIndexWithStyle(5, DefaultStyle1, "IR    : %02x [%s]", c.instructionRegister, instruction.INST_HUMAN_READABLE[c.instructionRegister])
 	c.LogfFlagIndexWithStyle(6, DefaultStyle1, "STEP  : %d/%d", visualStep, stepLen)
